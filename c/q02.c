0525fec8cbbe8caa1db2013df391f7893a3d5e90
ogsahil <contactsahilpnayak@gmail.com>
2023-09-03 20:39:16 +0530

    question 02

---
+++ c/q02.c
@@ -0,0 +1,16 @@
+// WAP to print from 1-10 using for loop
+
+#include <stdio.h>
+
+int main()
+{
+
+    int i;
+    for (i = 0; i <= 10; i++)
+    {
+
+        printf("here is the number from 1 to 10 : %d\n", i);
+    }
+
+    return 0;
+}
